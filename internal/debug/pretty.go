502bd9c85c3703767fd035285d0c67490489a33e
David Lazar <lazard@csail.mit.edu>
2017-07-13 11:49:53 -0400

    internal/debug: pretty-print ed25519 keys

--- internal/debug/pretty.go
+++ internal/debug/pretty.go
@@ -3,17 +3,23 @@ package debug
 import (
 	"reflect"
 
+	"golang.org/x/crypto/ed25519"
+
 	"github.com/davidlazar/go-crypto/encoding/base32"
 	"github.com/kylelemons/godebug/pretty"
 )
 
 func init() {
-	pretty.DefaultFormatter[reflect.TypeOf([]byte{})] = func(data []byte) string {
-		return "\"" + base32.EncodeToString(data) + "\""
-	}
+	pretty.DefaultFormatter[reflect.TypeOf([]byte{})] = prettyBytes
+	pretty.DefaultFormatter[reflect.TypeOf(ed25519.PrivateKey{})] = prettyBytes
+	pretty.DefaultFormatter[reflect.TypeOf(ed25519.PublicKey{})] = prettyBytes
 	pretty.DefaultFormatter[reflect.TypeOf([32]byte{})] = func(data [32]byte) string {
-		return "\"" + base32.EncodeToString(data[:]) + "\""
+		return prettyBytes(data[:])
 	}
+}
+
+func prettyBytes(data []byte) string {
+	return "\"" + base32.EncodeToString(data) + "\""
 }
 
 func Pretty(v interface{}) string {
