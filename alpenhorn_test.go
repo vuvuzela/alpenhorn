70bdefc21242e699a2a7e262270d451e0586b1d3
Claire Nord <cnord@mit.edu>
2019-05-09 02:36:46 -0400

    internal/mock: move packages to shared internal

--- alpenhorn_test.go
+++ alpenhorn_test.go
@@ -22,11 +22,11 @@ import (
 	"vuvuzela.io/alpenhorn/coordinator"
 	"vuvuzela.io/alpenhorn/edtls"
 	"vuvuzela.io/alpenhorn/internal/alplog"
-	"vuvuzela.io/alpenhorn/internal/mock"
 	"vuvuzela.io/alpenhorn/log"
 	"vuvuzela.io/alpenhorn/pkg"
 	"vuvuzela.io/crypto/rand"
 	"vuvuzela.io/internal/debug"
+	"vuvuzela.io/internal/mock"
 )
 
 type chanHandler struct {
